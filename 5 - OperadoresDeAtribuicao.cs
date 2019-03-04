2d0472606e23cb3d0ed45d268c2cbba0db8f60ce
igorjosiel <39222047+igorjosiel@users.noreply.github.com>
2019-03-04 14:20:08 -0300

    Fazendo alterações

--- 5 - OperadoresDeAtribuicao.cs
+++ 5 - OperadoresDeAtribuicao.cs
@@ -27,6 +27,7 @@ namespace OperadoresDeAtribuicao
 
             //Imprime o novo valor da variável a na tela
             Console.WriteLine(a);
+            Console.WriteLine("\n);
 
             //Declarando variável do tipo string e inicializando-a com ABC
             string s = "ABC";
@@ -39,6 +40,7 @@ namespace OperadoresDeAtribuicao
 
             //Imprime o novo valor da variável s
             Console.WriteLine(s);
+            Console.WriteLine("\n");
 
             //Declarando variável do tipo int e inicializando-a com o valor 10
             int b = 10;
@@ -54,6 +56,7 @@ namespace OperadoresDeAtribuicao
 
             //Imprime o novo valor da variável b na tela
             Console.WriteLine(b);
+            Console.WriteLine("\n");
 
             //Declarando variável do tipo int e inicializando-a com o valor 10
             int val1 = 10;
@@ -63,6 +66,7 @@ namespace OperadoresDeAtribuicao
 
             //Imprime a frase na tela com os valores das variáveis val1 e val2 utilizando o método de impressão Placeholders
             Console.WriteLine("Valor de val1 e {0} e o valor de val2 e {1}", val1, val2);
+            Console.WriteLine("\n");
 
             //Declarando variável do tipo int e inicializando-a com o avlor 10
             int num1 = 10;
@@ -72,6 +76,7 @@ namespace OperadoresDeAtribuicao
 
             //Imprime a frase na tela com os valores das variáveis num1 e num2 utilizando o método de impressão Placeholders
             Console.WriteLine("Valor de num1 e {0} e o valor de num2 e {1}", num1, num2);
+            Console.WriteLine("\n");
 
         }//Fim do método Main
     }//Fim da classe OperadorDeAtribuicao
