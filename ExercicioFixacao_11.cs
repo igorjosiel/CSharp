fb673c193d10b7b98935da9d4ebc51da44b37fdd
igorjosiel <39222047+igorjosiel@users.noreply.github.com>
2019-03-17 11:53:01 -0300

    adicionando aos comentários

--- ExercicioFixacao_11.cs
+++ ExercicioFixacao_11.cs
@@ -20,7 +20,7 @@ namespace ExercicioFixacao11
             //Usando o método if-else encadeado para testar as condições. Se a condição do comando if for verdadeira, executa o bloco de instruções e termina o método, senão, pula para o próximo teste
             if (numero >= 0 && numero <= 25)
             {
-                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número
+                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número e com a quebra de linha
                 Console.WriteLine("\nO numero " + numero.ToString(CultureInfo.InvariantCulture) + " se encontra no intervalo [0, 25].\n");
 
             }//Fim do comando if do método if-else encadeado
@@ -28,7 +28,7 @@ namespace ExercicioFixacao11
             //Se a condição anterior foi falsa, testa a condição do comando else if a seguir. Se for verdadeira executa o bloco de instruções e termina o método, senão, pula para o próximo teste
             else if (numero > 25 && numero <= 50)
             {
-                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número
+                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número e com a quebra de linha
                 Console.WriteLine("\nO numero " + numero.ToString(CultureInfo.InvariantCulture) + " se encontra no intervalo [25, 50].\n");
 
             }//Fim do comando else if do método if-else encadeado
@@ -36,7 +36,7 @@ namespace ExercicioFixacao11
             //Se a condição anterior foi falsa, testa a condição do comando else if a seguir. Se for verdadeira executa o bloco de instruções e termina o método, senão, pula para o próximo teste
             else if (numero > 50 && numero <= 75)
             {
-                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número
+                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número e com a quebra de linha
                 Console.WriteLine("\nO numero " + numero.ToString(CultureInfo.InvariantCulture) + " se encontra no intervalo [50, 75].\n");
 
             }//Fim do comando else if do método if-else encadeado
@@ -44,7 +44,7 @@ namespace ExercicioFixacao11
             //Se a condição anterior foi falsa, testa a condição do comando else if a seguir. Se for verdadeira executa o bloco de instruções e termina o método, senão, pula para o próximo teste
             else if (numero > 75 && numero <= 100)
             {
-                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número
+                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número e com a quebra de linha
                 Console.WriteLine("\nO numero " + numero.ToString(CultureInfo.InvariantCulture) + " se encontra no intervalo [75, 100].\n");
 
             }//Fim do comando else if do método if-else encadeado
@@ -52,7 +52,7 @@ namespace ExercicioFixacao11
             //Se a condição anterior foi falsa, executa o bloco de instruções do comando else
             else
             {
-                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número
+                //Imprime a frase na tela com o valor da variável numero e utilizando o método ToString(CultureInfo.InvariantCulture) para fixar o ponto separador da parte inteira da parte decimal do número e com a quebra de linha
                 Console.WriteLine("\nO numero " + numero.ToString(CultureInfo.InvariantCulture) + " se encontra fora dos intervalos.\n");
 
             }//Fim do comando else do método if-else encadeado
