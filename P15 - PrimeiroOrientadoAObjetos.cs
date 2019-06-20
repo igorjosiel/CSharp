b9116a55dfe310f6f182a50a5eccaa7af5b3d21b
Igor Josiel <igorjosielvitalino@gmail.com>
2019-06-19 21:06:50 -0300

    Novos arquivos

---
+++ P15 - PrimeiroOrientadoAObjetos.cs
@@ -0,0 +1,66 @@
+﻿//Utilizando os namespace System e System.Globalization
+using System;
+using System.Globalization;
+
+//Usando o namespace PrimeiroOrientadoAObjetos
+namespace PrimeiroOrientadoAObjetos
+{
+    //Classe PrimeiroOrientadoAObjetos
+    class PrimeiroOrientadoAObjetos
+    {
+        //Método principal Main
+        static void Main(String[] args)
+        {
+            //Declarando dois objetos do tipo Triangulo
+            Triangulo x, y;
+
+            //Instanciando os dois objetos com a palavra-chave new
+            x = new Triangulo();
+            y = new Triangulo();
+
+            //Imprime a frase na tela
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+
+            //Atribuindo valores aos atributos do objeto x
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Imprime a frase na tela
+            Console.WriteLine("\nEntre com as medidas do triangulo Y: ");
+
+            //Atribuindo valores aos atributos do objeto y
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Declarando variável areaX do tipo double para armazenar a area do objeto x
+            double areaX = x.Area();
+
+            //Declarando variável areaY do tipo double para armazenar a area do objeto y
+            double areaY = y.Area();
+
+            //Imprime as frases na tela com as respectivas formatações
+            Console.WriteLine("\nArea de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("\nArea de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            //Estrutura condicional if-else composto
+            //Se o valor da variável areaX for maior que o valor da variável areaY executa o bloco de instruções abaixo
+            if (areaX > areaY)
+            {
+                //Imprime a frase na tela
+                Console.WriteLine("\nMaior area: X");
+
+            }//Fim do comando if da estrutura condicional if-else composto
+
+            //Senão, executa o bloco de intruções abaixo
+            else
+            {
+                //Imprime a frase na tela
+                Console.WriteLine("\nMaior area: Y");
+
+            }//Fim do comando else da estrutura condicional if-else composto
+
+        }//Fim do método Main
+    }//Fim da classe PrimeiroOrientadoAObjetos
+}//Fim do namespace PrimeiroOrientadoAObjetos
---
+++ P17 - MembrosEstaticos.cs
@@ -0,0 +1,31 @@
+﻿//Utilizando os namespaces System e System.Globalization
+using System;
+using System.Globalization;
+
+//Usando o namespace MembrosEstaticos
+namespace MembrosEstaticos
+{
+    //Classe MembrosEstaticos
+    class MembrosEstaticos
+    {
+        //Método principal Main
+        static void Main(string[] args)
+        {
+            //Imprime a frase na tela
+            Console.Write("Entre com o valor do raio: ");
+
+            //Declarando variável do tipo double e inicializando-a com um valor digitado pelo usuário
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Declarando variáveis do tipo double e inicializando-as com os resultados das chamadas aos métodos Circunferencia e volume passando como parâmetro o valor da variável raio
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            //Imprime as frases na tela com as devidas formatações e valores das variáveis circ, volume e PI
+            Console.WriteLine("\nCircunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.PI.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }//Fim do método Main
+    }//Fim da classe MembrosEstaticos
+}//Fim do namespace MembrosEstaticos
